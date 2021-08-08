4563f4a9ca9306a8105d08553c882ba77258a5dd
lion-oldriver <w879443a72cj2i@gmail.com>
2021-08-08 05:33:21 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '382098aba676cf039f5c8afe3d20f3424f2deec5379dfd8824e00acc4ce76742e371ea22b7f14274199176061896da4e6e5161a994bb86b66b2ab7ecd50e4819'
